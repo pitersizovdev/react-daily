cea8dcc021cd634699134b60e310c82e37d9cac5
PAKOKK <pavel-kalinin2015@mail.ru>
2024-04-09 21:08:58 +0300

    0.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import "./App.css";
 import Button from "./components/Button/Button";
-import JournalItem from './components/JurnalItem/JournalItem';
+import JournalItem from "./components/JurnalItem/JournalItem";
 
 function App() {
+  const data = [
+    {
+      title: "Задача первая",
+      text: "Текст первой задачи расположен здесь",
+      date: new Date(),
+    },
+    {
+      title: "Задача вторая",
+      text: "Текст второй задачи расположен здесь",
+      date: new Date(),
+    },
+  ];
   return (
     <>
       <Button />
-      <JournalItem/>
+      <JournalItem
+        title={data[0].title}
+        text={data[0].text}
+        date={data[0].date}
+      />
+      <JournalItem
+        title={data[1].title}
+        text={data[1].text}
+        date={data[1].date}
+      />
     </>
   );
 }
--- src/components/JurnalItem/JournalItem.jsx
+++ src/components/JurnalItem/JournalItem.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import "./journalItem.css";
 
-const JournalItem = () => {
-  const title = "Задача первая";
-  const date = new Date();
-  const text = "Текст первой задачи расположен здесь";
+const JournalItem = ({ title, text, date }) => {
+  const stringDate = new Intl.DateTimeFormat("ru-RU").format(date);
 
   return (
     <div className="journal-item">
       <h2 className="journal-item__header">{title}</h2>
       <h2 className="journal-item__body">
-        <div className="journal-item__date">{date.toDateString()}</div>
-        <div className="journal-text">{text}</div>
+        <div className="journal-item__date">{stringDate}</div>
+        <div className="journal-item__text">{text}</div>
       </h2>
     </div>
   );
